0cf63f41b38452b573b9bd9a78fe8017a79f6cca
katsutsani <katsutsani@gmail.com>
2023-03-31 23:51:26 +0200

    Menu

---
+++ Galaxy Crunshers/Assets/Script/Move.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float Speed;
+
+    public int _delayDirection = 5;
+    public int _delayStop = 5;
+    private float x;
+    private float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(OnMove());
+    }
+
+    // Update is called once per frame
+
+    IEnumerator Idle()
+    {
+        yield return new WaitForSeconds(_delayStop);
+        StartCoroutine(OnMove());
+    }
+
+    IEnumerator OnMove()
+    {
+        float count = 0;
+        ChangeDir();
+        while (count < _delayDirection)
+        {
+            count += Time.deltaTime;
+            transform.position += Time.deltaTime * Speed * new Vector3(x, y);
+            yield return null;
+            
+        }
+        StartCoroutine(Idle());
+    }
+
+    void ChangeDir()
+    {
+        int direction = UnityEngine.Random.Range(0, 3);
+        switch (direction)
+        {
+            case 0:
+                x = -0.5f;
+                y = -0.25f;
+                break;
+            case 1:
+                x = 0.5f;
+                y = 0.25f;
+                break;
+            case 2:
+                x = -0.5f;
+                y = 0.25f;
+                break;
+            case 3:
+                x = 0.5f;
+                y = -0.25f;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Galaxy Crunshers/Assets/ShowShop.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowShop : MonoBehaviour
+{
+    [SerializeField] Button Ui;
+    [SerializeField] TextMeshPro ShopTitle;
+    [SerializeField] GameObject ShopBackground;
+
+    void OpenMenu()
+    {
+        Ui.;
+        ShopTitle.GetComponent;
+    }
+}
