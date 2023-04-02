fb6013cf12342e9d2ad7728d52ffbdac0b353698
katsutsani <katsutsani@gmail.com>
2023-04-02 03:13:39 +0200

    gameInfo

--- Galaxy Crunshers/Assets/Script/GameInfo.cs
+++ Galaxy Crunshers/Assets/Script/GameInfo.cs
@@ -3,4 +3,15 @@ using UnityEngine;
 
 public class GameInfo : MonoBehaviour
 {
+    [SerializeField] GameObject AllCharacter;
+    [SerializeField] GameObject AllBuilding;
+
+
+    public void compteCharacter()
+    {
+        foreach (Transform item in AllCharacter.GetComponentInChildren<Transform>())
+        {
+            Debug.Log("test");
+        }
+    }
 }
