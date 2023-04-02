23245dc8d7aebf385a627cb7ca8b9dcb69d83284
katsutsani <katsutsani@gmail.com>
2023-04-02 16:28:02 +0200

    Merge branch 'main' of https://github.com/katsutsani/Galaxy-Crunshers

--- Galaxy Crunshers/Assets/Rage.cs
+++ Galaxy Crunshers/Assets/Rage.cs
@@ -14,18 +14,21 @@ public class Rage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GameInfo.GetComponent<GameInfo>().Paysan >= GameInfo.GetComponent<GameInfo>().Champ+2)
+        if (GameInfo.GetComponent<GameInfo>().Paysan > 1)
         {
-            //if (Random.Range(0, 10) == 1)
-            //{
-            //    Transform[] array = GameInfo.GetComponent<GameInfo>()._allCharacter.GetComponentsInChildren<Transform>();
-            //    for (int i = GameInfo.GetComponent<GameInfo>().Paysan - GameInfo.GetComponent<GameInfo>().Champ; i >= 1; i--)
-            //    {
-            //        array[i].gameObject.GetComponent<SpriteRenderer>().color = Color.red;
-            //        array[i].gameObject.GetComponent<Move>().Speed = 20;
-            //    }
-            //}
+            if (GameInfo.GetComponent<GameInfo>().Paysan > GameInfo.GetComponent<GameInfo>().Champ + 2)
+            {
+                if (Random.Range(0, 10) == 1)
+                {
+                    Rage[] array = GameInfo.GetComponent<GameInfo>()._allCharacter.GetComponentsInChildren<Rage>();
+                    for (int i = GameInfo.GetComponent<GameInfo>().Paysan - (GameInfo.GetComponent<GameInfo>().Champ + 1); i >= 1; i--)
+                    {
+                        array[i].gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+                        array[i].gameObject.GetComponent<Move>().Speed = 20;
+                    }
+                }
 
+            }
         }
     }
 }
--- Galaxy Crunshers/Assets/Script/UseDiamonds.cs
+++ Galaxy Crunshers/Assets/Script/UseDiamonds.cs
@@ -6,13 +6,24 @@ public class UseDiamonds : MonoBehaviour
     [SerializeField] private GameObject m_Prefab;
     public static event Action<bool> StopTimer;
     private bool timerSet = false;
+    private Building _buildScript;
+    [SerializeField] public SpriteRenderer _spriteR;
+
+    public static event Action<bool> Timer;
+
+    private void Start()
+    {
+        _buildScript = GetComponent<Building>();
+    }
 
     private void OnMouseDown()
     {
         if (gameObject.tag == "Diamond")
         {
             timerSet = true;
+            Timer?.Invoke(timerSet);
             StopTimer?.Invoke(timerSet);
+            _buildScript._particle.Play();
         }
     }
 }
