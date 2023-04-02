a878f8095d3e1cf0da536f8af1a1d0fcaaf9a74d
katsutsani <katsutsani@gmail.com>
2023-04-02 03:06:44 +0200

    ajout prefab

--- Galaxy Crunshers/Assets/AddCharacter.cs
+++ Galaxy Crunshers/Assets/AddCharacter.cs
@@ -34,12 +34,18 @@ public class AddCharacter : MonoBehaviour
                 break;
             case "1Character":
                 _instanceCharacter = Instantiate(_character[1]);
+                price = 100;
+                OnCharacterAdded?.Invoke(price);
                 break;
             case "2Character":
                 _instanceCharacter = Instantiate(_character[2]);
+                price = 100;
+                OnCharacterAdded?.Invoke(price);
                 break;
             case "3Character":
                 _instanceCharacter = Instantiate(_character[3]);
+                price = 100;
+                OnCharacterAdded?.Invoke(price);
                 break;
 
         }
